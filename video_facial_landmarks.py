df734f7491ff5a6b97484ffa576decd4a6a95c75
Supakorn Wongsawang <supakorn.wo@ku.th>
2019-12-24 23:47:33 +0700

    calculate size, score(that i don't know what is)

--- video_facial_landmarks.py
+++ video_facial_landmarks.py
@@ -7,48 +7,57 @@ import imutils
 import time
 import dlib
 import cv2
- 
-# construct the argument parse and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-r", "--picamera", type=int, default=-1, help="whether or not the Raspberry Pi camera should be used")
-args = vars(ap.parse_args())
- 
-# initialize dlib's face detector (HOG-based) and then create
-# the facial landmark predictor
-print("[INFO] loading facial landmark predictor...")
-detector = dlib.get_frontal_face_detector()
-# predictor = dlib.shape_predictor(args["shape_predictor"])
 
-# initialize the video stream and allow the cammera sensor to warmup
-print("[INFO] camera sensor warming up...")
-vs = VideoStream(usePiCamera=args["picamera"] > 0).start()
-time.sleep(2.0)
 
-# loop over the frames from the video stream
-while True:
-    # grab the frame from the threaded video stream, resize it to
-    # have a maximum width of 400 pixels, and convert it to
-    # grayscale
-    frame = vs.read()
-    frame = imutils.resize(frame, width=400)
-    # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    dets,scores,idx = detector.run(frame, 0)
-    for i, d in enumerate(dets):
-        font = cv2.FONT_HERSHEY_DUPLEX
-        text_showed = "{}".format(idx[i])
-        print("Detection {}: Left: {} Top: {} Right: {} Bottom: {} IDX:{} Score:{}".format(
-            i, d.left(), d.top(), d.right(), d.bottom(),idx[i],scores[i]))
-        cv2.rectangle(frame, (d.left(), d.top()), (d.right(), d.bottom()), (0, 0, 255), 2)
-        cv2.putText(frame, text_showed, (d.left() + 6, d.bottom() - 6),
-                    font, 0.5, (255, 255, 255), 1)
+def calculate_size(d):
+    return (d.bottom()-d.top()) * (d.right()-d.left())
 
-    # show the frame
-    cv2.imshow("Frame", frame)
-    key = cv2.waitKey(1) & 0xFF
-    # if the `q` key was pressed, break from the loop
-    if key == ord("q"):
-        break
- 
-# do a bit of cleanup
-cv2.destroyAllWindows()
-vs.stop()
+
+if __name__ == "__main__":
+    # construct the argument parse and parse the arguments
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-r", "--picamera", type=int, default=-1,
+                    help="whether or not the Raspberry Pi camera should be used")
+    args = vars(ap.parse_args())
+
+    # initialize dlib's face detector (HOG-based) and then create
+    # the facial landmark predictor
+    print("[INFO] loading facial landmark predictor...")
+    detector = dlib.get_frontal_face_detector()
+    # predictor = dlib.shape_predictor(args["shape_predictor"])
+
+    # initialize the video stream and allow the cammera sensor to warmup
+    print("[INFO] camera sensor warming up...")
+    vs = VideoStream(usePiCamera=args["picamera"] > 0).start()
+    time.sleep(2.0)
+
+    # loop over the frames from the video stream
+    while True:
+        # grab the frame from the threaded video stream, resize it to
+        # have a maximum width of 400 pixels, and convert it to
+        # grayscale
+        frame = vs.read()
+        frame = imutils.resize(frame, width=400)
+        # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        dets, scores, idx = detector.run(frame, 0)
+        for i, d in enumerate(dets):
+            font = cv2.FONT_HERSHEY_DUPLEX
+            text_showed = "{} {:0.2f} {:0.2f}".format(
+                idx[i], scores[i], calculate_size(d))
+            print("Detection {}: Left: {} Top: {} Right: {} Bottom: {} IDX:{} Score:{} Size:{}".format(
+                i, d.left(), d.top(), d.right(), d.bottom(), idx[i], scores[i], calculate_size(d)))
+            cv2.rectangle(frame, (d.left(), d.top()),
+                          (d.right(), d.bottom()), (0, 0, 255), 2)
+            cv2.putText(frame, text_showed, (d.left() + 6, d.bottom() - 6),
+                        font, 0.5, (255, 255, 255), 1)
+
+        # show the frame
+        cv2.imshow("Frame", frame)
+        key = cv2.waitKey(1) & 0xFF
+        # if the `q` key was pressed, break from the loop
+        if key == ord("q"):
+            break
+
+    # do a bit of cleanup
+    cv2.destroyAllWindows()
+    vs.stop()
