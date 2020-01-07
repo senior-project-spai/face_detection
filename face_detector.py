9cc3b8a4f214902a61701db3ab1e5bbec4c8911e
Supakorn Wongsawang <supakorn.wo@ku.th>
2020-01-08 03:53:05 +0700

    s3

--- face_detector.py
+++ face_detector.py
@@ -14,6 +14,19 @@ import requests
 import json
 usePiCamera = False
 
+BRANCH_ID = 123456
+CAMERA_ID = 123456
+
+
+best_frame = None
+best_frame_reduced = None
+max_confidence = -1
+best_frame_buttom = -1
+best_frame_right = -1
+best_frame_top = -1
+best_frame_left = -1
+best_frame_size = -1
+
 app = FastAPI()
 
 
@@ -65,19 +78,74 @@ def get_biggest_face(dets, scores, idx):
 def uploadS3(frame):
     # Uploading to S3
     print("Uploading Frame to S3")
-
+    currentTime = int(time.time())
+    pictureName = "FaceDetector"+str(BRANCH_ID)+"_"+str(CAMERA_ID)+"_"+str(currentTime)+".jpg"
     data = {
-        'time': int(time.time()),
-        'branch_id': 123456,
-        'camera_id': 123456,
-        'picture': frame.tostring(),
-        'pictureName': 'Best-test-123456.jpg',
+        'time': currentTime,
+        'branch_id': BRANCH_ID,
+        'camera_id': CAMERA_ID,
+        # 'picture': frame.tostring(),
+        'pictureName': pictureName,
     }
-
+    file = {'picture': (pictureName, frame.tostring(),
+                        'image/jpeg', {'Expires': '0'})}
     response = requests.post(
-        "https://image-to-s3-spai.apps.spai.ml/_api/image", data=data)
+        "https://image-to-s3-spai.apps.spai.ml/_api/image", files=file, data=data)
 
     print(json.loads(response.text))
+
+
+def detection(detector, frame):
+    # The score is bigger for more confident detections.
+    dets, scores, idx = detector.run(frame, 0)
+    det, score, idx, size = get_biggest_face(dets, scores, idx)
+
+    # det == -1 means not found face in picture
+    if det != -1:
+        print("Left: {} Top: {} Right: {} Bottom: {} IDX:{} Score:{} Size:{}".format(
+            det.left(), det.top(), det.right(), det.bottom(), idx, score, size))
+        response = {
+            'left': det.left(),
+            'top': det.top(),
+            'right': det.right(),
+            'bottom': det.bottom(),
+            'score': score,
+            'size': size
+        }
+        return response
+    else:
+        print("Face Not Found")
+        return {}
+
+
+def detections(detector, frame):
+    global best_frame
+    global best_frame_reduced
+    global max_confidence
+    global best_frame_buttom
+    global best_frame_right
+    global best_frame_top
+    global best_frame_left
+    global best_frame_size
+
+    raw_frame = frame
+    frame = imutils.resize(frame, width=400)
+    raw_frame_reduced = frame
+    det = detection(detector, frame)
+    if det != {}:
+        if det['score'] > max_confidence:
+            print("BEST!!!!!!!!!!!!!")
+            best_frame = raw_frame
+            best_frame_reduced = raw_frame_reduced
+
+            max_confidence = det['score']
+            best_frame_buttom = det['bottom']
+            best_frame_right = det['right']
+            best_frame_top = det['top']
+            best_frame_left = det['left']
+            best_frame_size = det['size']
+    else:
+        print("Face Not Found")
 
 
 @app.post("/detection", response_model=Idetection_response)
@@ -93,52 +161,24 @@ async def trigger_detection(body: Itrigger_detection):
     vs = VideoStream(usePiCamera=usePiCamera, resolution=(960, 720)).start()
     time.sleep(2.0)
 
-    best_frame = None
-    best_frame_reduced = None
-    max_confidence = -1
-    best_frame_buttom = -1
-    best_frame_right = -1
-    best_frame_top = -1
-    best_frame_left = -1
-    best_frame_size = -1
-
     # loop frame by frame from video stream
     for _ in range(0, 50):
         # grab the frame from the threaded video stream,
         # resize it to maximum width of 400 pixels
         frame = vs.read()
-        raw_frame = frame
-        frame = imutils.resize(frame, width=400)
-        raw_frame_reduced = frame
-
-        h, w = get_height_frame(frame), get_width_frame(frame)
-
-        # The score is bigger for more confident detections.
-        dets, scores, idx = detector.run(frame, 0)
-        det, score, idx, size = get_biggest_face(dets, scores, idx)
-
-        # det == -1 means not found face in picture
-        if det != -1:
-            print("Left: {} Top: {} Right: {} Bottom: {} IDX:{} Score:{} Size:{}".format(
-                det.left(), det.top(), det.right(), det.bottom(), idx, score, size))
-
-            if score > max_confidence:
-                print("BEST!!!!!!!!!!!!!")
-                best_frame = raw_frame
-                best_frame_reduced = raw_frame_reduced
-                max_confidence = score
-                best_frame_buttom = det.bottom()
-                best_frame_right = det.right()
-                best_frame_top = det.top()
-                best_frame_left = det.left()
-                best_frame_size = size
-        else:
-            print("Face Not Found")
+        detections(detector, frame)
 
     # Save Best frame
     cv2.imwrite("Best.jpg", best_frame)
     cv2.imwrite("Best_reduced.jpg", best_frame_reduced)
     _, best_frame_encoded = cv2.imencode('.jpg', best_frame)
+    det = detection(detector, best_frame)
+    max_confidence = det['score']
+    best_frame_buttom = det['bottom']
+    best_frame_right = det['right']
+    best_frame_top = det['top']
+    best_frame_left = det['left']
+    best_frame_size = det['size']
     print("Best Frame Left: {} Top: {} Right: {} Bottom: {} Score:{} Size:{}".format(
         best_frame_left, best_frame_top, best_frame_right, best_frame_buttom, max_confidence, best_frame_size))
 
